1c6d125e7c67af3a931233b88566e5b2475e1359
Puneet Vishnoi <puneetvishnoi2017@gmail.com>
2023-09-20 13:40:19 +0530

    create routes and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route ,Routes} from 'react-router-dom'
 import { useState } from "react";
 import Header from "./component/Header";
 import FeedbackList from "./component/FeedbackList";
 import FeedbackStats from './component/FeedbackStats'
 import FeedbackData from "./data/FeedbackData";
 import FeedbackForm from "./component/FeedbackForm";
-
+import AboutComponentIcon from './component/AboutComponentIcon';
+import AboutPages from "./component/pages/AboutPage"
 
 function App(){
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -30,14 +32,26 @@ function App(){
   }
 
   return (
-    <>
+    <Router>
       <Header text="Feeback UI" />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleClick={deleteItem}/>
-      </div>
-    </>
+      <Routes>
+        <Route exact path ='/' element={
+          <>
+            <div className="container">
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList feedback={feedback} handleClick={deleteItem}/>
+            </div>
+            <AboutComponentIcon/>
+          </>
+        }></Route>
+        <Route path='/about' element={
+          <div className='container'>
+            <AboutPages/>
+          </div>
+        }></Route>
+      </Routes>
+    </Router>
   )
 }
 
